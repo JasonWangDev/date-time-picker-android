c1579a295266a84b685cabebe430ab0c0bbdbc14
DOITSKY\jason <jasonwang@doitsky.com>
2017-06-29 15:56:07 +0800

    Update DatePickerDialogFragment

--- datetimepicker-dialogfragment-android/src/main/java/com/github/jasonwangdev/datetimepicker/DatePickerDialogFragment.java
+++ datetimepicker-dialogfragment-android/src/main/java/com/github/jasonwangdev/datetimepicker/DatePickerDialogFragment.java
@@ -1,8 +1,6 @@
 package com.github.jasonwangdev.datetimepicker;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
